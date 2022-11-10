acea9ec027c61137ebe44912489a3eb59557e807
Daniel <107085390+Daniel-Alves27@users.noreply.github.com>
2022-11-10 19:38:24 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,103 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+function HomePage() {
+    //console.log(config.playlists);
+    //const estiloDaHomePage = { background: "red" };
+
+    return (
+        <>
+            <CSSReset/>
+            <div>
+                <Menu />
+                <Header />
+                <TimeLine playlists={config.playlists}>
+                    conteudo
+                </TimeLine>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
+
+/*function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}*/
+
+const StyledHeader = styled.div`
+    img {
+        margin-top: 50px;
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+        align-items: flex-end;
+    }
+`;
+function Header() {
+    return (
+        <StyledHeader>
+
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
+            </section>
+        </StyledHeader>
+    )
+}
+
+function TimeLine(propriedade) {
+    console.log("Dentro do componente", propriedade);
+
+    const playlistNames = Object.keys(propriedade.playlists);
+
+
+    return (
+        <StyledTimeline>
+            {playlistNames.map((playlistName) => {
+                const videos = propriedade.playlists[playlistName];
+                console.log(playlistName);
+                console.log(videos);
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos.map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img src={video.thumb} />
+                                        <span>
+                                            {video.title}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
